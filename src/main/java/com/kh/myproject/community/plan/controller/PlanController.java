e179bbea255d056c2f2072cf3e72275d97723948
JUNGWOOKCHOI <cjw9796@gmail.com>
2023-08-29 11:42:45 +0900

    카카오맵 수정중

--- src/main/java/com/kh/myproject/community/plan/controller/PlanController.java
+++ src/main/java/com/kh/myproject/community/plan/controller/PlanController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class PlanController {
 
     //일정 리스트(일정 메인)
-    @GetMapping("/community/plan") // c
+    @GetMapping("/community/plan") // http://localhost:8080/community/plan
     public String communityplan() {
 
           return "community/plan/plan";
@@ -17,7 +17,7 @@ public class PlanController {
     }
 
     //일정 글 정보
-    @GetMapping("/community/plan/detail") // http://localhost:8070/community/plan/detail
+    @GetMapping("/community/plan/detail") // http://localhost:8080/community/plan/detail
     public String communityplandetail() {
 
 
@@ -25,29 +25,16 @@ public class PlanController {
     }
 
     //일정 글 쓰기
-    @GetMapping("/community/plan/write") // http://localhost:8070/community/plan/write
+    @GetMapping("/community/plan/write") // http://localhost:8080/community/plan/write
     public String communityplanwrite() {
 
 
         return "community/plan/plan_write";
     }
 
-    //일정 글 쓰기 카카오맵 실험
-    @GetMapping("/community/plan/write_kakaomap") // http://localhost:8070/community/plan/write_kakaomap
-    public String communityplanwritekakao() {
 
 
-        return "community/plan/plan_write_kakaomap";
-    }
 
-    /*
-    //일정 글 정보
-    @GetMapping("/community/plan/search") // http://localhost:8070/community/plan/search
-    public String communityplansearch() {
-
-        return "community/plan/plan_search";
-    }
-     */
 
 
 
