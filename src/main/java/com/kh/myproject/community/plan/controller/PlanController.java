2c2b6d06271a1f05936f8c0fe16fc628e00e81f2
JUNGWOOKCHOI <cjw9796@gmail.com>
2023-09-05 15:36:27 +0900

    Plan_Write DB작업 완료

--- src/main/java/com/kh/myproject/community/plan/controller/PlanController.java
+++ src/main/java/com/kh/myproject/community/plan/controller/PlanController.java
@@ -1,12 +1,20 @@
 package com.kh.myproject.community.plan.controller;
 
 
+import com.kh.myproject.community.plan.model.dto.PlanBoardDTO;
 import com.kh.myproject.community.plan.model.dto.PlanBoardDetailDTO;
-import com.kh.myproject.community.plan.model.dto.PlanBoardDetailDTO;
+import com.kh.myproject.community.plan.model.entity.PlanBoard;
+import com.kh.myproject.community.plan.model.entity.PlanBoardDetail;
+import com.kh.myproject.community.plan.repository.PlanBoardDetailRepository;
+import com.kh.myproject.community.plan.repository.PlanBoardRepository;
+import com.kh.myproject.community.plan.service.PlanBoardService;
+import com.kh.myproject.member.user.model.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.*;
 
@@ -18,43 +26,39 @@ import static java.lang.Integer.parseInt;
 public class PlanController {
 
 
-    private Map<Integer, ArrayList<PlanBoardDetailDTO>> planMap = new HashMap<>();
+    @Autowired
+    PlanBoardService planBoardService;
+
+//    private Map<Integer, ArrayList<PlanBoardDetailDTO>> planMap = new HashMap<>();
+    private List<PlanBoardDetailDTO> planList = new ArrayList<>();
 
     //일정 리스트(일정 메인)
     @GetMapping("/community/plan") // http://localhost:8080/community/plan
     public String communityplan() {
 
         return "community/plan/plan";
-
     }
 
     //일정 글 정보
     @GetMapping("/community/plan/detail") // http://localhost:8080/community/plan/detail
     public String communityplandetail() {
 
-        //int result = service.save(entity);
-        /*
-        if (result == 1){
-
-        planList.clear();
-        }
-
-         */
-
         return "community/plan/plan_detail";
     }
 
     //일정 글 쓰기
     @GetMapping("/community/plan/write") // http://localhost:8080/community/plan/write
-    public String communityplanwrite(Model model) {
+    public String communityplanwrite(Model model, @ModelAttribute("user")User user) {
 
-        System.out.println(planMap);
+        model.addAttribute(user);
+
+        System.out.println(planList);
         int maxDay = 0;
-        if (!planMap.isEmpty()) {
-            model.addAttribute("planMap", planMap);
-            for (int day : planMap.keySet()) {
-                if (day > maxDay) {
-                    maxDay = day;
+        if (!planList.isEmpty()) {
+            model.addAttribute("planList", planList);
+            for(int i = 0; i < planList.size(); i++){
+                if(planList.get(i).getPbdDate() > maxDay){
+                    maxDay = planList.get(i).getPbdDate();
                 }
             }
             model.addAttribute("maxDay", maxDay);
@@ -80,29 +84,46 @@ public class PlanController {
     @ResponseBody
     public void move(@RequestBody PlanBoardDetailDTO[] dtoList) {
 
-        ArrayList<PlanBoardDetailDTO> planList = new ArrayList<>();
 
         for (PlanBoardDetailDTO PlanBoardDetailDTO : dtoList) {
             planList.add(PlanBoardDetailDTO);
         }
-        planMap.put(dtoList[0].getPbdDate(), planList);
 
 
     }
 
-    //일정 삭제하는 컨트롤러
+    //일정 삭제하는 컨트롤러(Plan_write에서 삭제버튼 클릭 시)
     @PostMapping("/community/plan/deletePlan")
     @ResponseBody
     public void deletePlan(@RequestParam("day") String day, @RequestParam("placeName") String placeName){
 
-        ArrayList<PlanBoardDetailDTO> temp = planMap.get(parseInt(day));
-        for(int i = 0; i< temp.size(); i++ ){
-            System.out.println(temp.get(i));
-            if(temp.get(i).getPbdPlaceName().equals(placeName)){
-                temp.remove(i);
+        for(int i = 0; i< planList.size(); i++ ){
+
+            if(planList.get(i).getPbdPlaceName().equals(placeName) && planList.get(i).getPbdDate() == parseInt(day)){
+                planList.remove(i);
             }
         }
-        planMap.put(parseInt(day) , temp);
+    }
+
+
+    //Plan_write 작성 완료 버튼 클릭 시
+    @PostMapping("/community/plan/completePlan")
+    @ResponseBody
+    public String completePlan(@RequestBody PlanBoardDTO boardDTO,
+                               @ModelAttribute("user") User user) {
+
+
+
+        // 1. Dto user 정보 저장
+        boardDTO.setUser(user);
+
+        // 2. service에서 db에 저장할때 필요한거 다 넘겨줌
+        planBoardService.savePlanBoard(boardDTO, planList);
+
+        // 9. 저장이 완료되면 planList 초기화
+        planList.clear();
+
+        return "community/plan/plan";
     }
 
 
--- src/main/java/com/kh/myproject/community/plan/model/dto/PlanBoardDetailDTO.java
+++ src/main/java/com/kh/myproject/community/plan/model/dto/PlanBoardDetailDTO.java
@@ -13,12 +13,12 @@ import lombok.*;
 @EqualsAndHashCode
 @Builder
 public class PlanBoardDetailDTO {
-    private Long pbdNum;
-    private String pbdPlaceName;
-    private String pbdCategoryGroupName;
-    private double pbdX;
-    private double pbdY;
-    private int pbdDate;
+    private Long pbdNum; //세부일정번호
+    private String pbdPlaceName; //여행지명
+    private String pbdCategoryGroupName; //장소분류명
+    private double pbdX; //x좌표
+    private double pbdY; //y좌표
+    private int pbdDate; //일자번호()
     private PlanBoard planBoard;
 
     public PlanBoardDetail toEntity(){
